e712030d399488b8fe4a8d6c50339845ae6fd8f1
fanfeiyang <zedfan@qq.com>
2025-01-12 09:50:48 +0800

    should_parse_multi_option

--- src/main/java/org/example/Args.java
+++ src/main/java/org/example/Args.java
@@ -1,36 +1,44 @@
 package org.example;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Args {
     public static <T> T parse(Class<T> singleOptionClass, String... args) {
         try {
             Constructor<?> constructor = singleOptionClass.getDeclaredConstructors()[0];
-            Parameter parameter = constructor.getParameters()[0];
-            Option option = parameter.getAnnotation(Option.class);
+            Parameter[] parameters = constructor.getParameters();
             List<String> arguments = Arrays.asList(args);
 
-            Class<?> type = parameter.getType();
-            if (type.equals(int.class)) {
-                int idx = arguments.indexOf("-" + option.value());
-                String s = arguments.get(idx + 1);
-                return (T) constructor.newInstance(Integer.valueOf(s));
-            } if (type.equals(String.class)){
-                int idx = arguments.indexOf("-" + option.value());
-                String s = arguments.get(idx + 1);
-                return (T) constructor.newInstance(String.valueOf(s));
-
-            }else {
-                boolean contains = arguments.contains("-" + option.value());
-                return (T) constructor.newInstance(contains);
-            }
+            Object[] argsForConstructor = Arrays.stream(parameters)
+                    .map(parameter -> value(parameter, arguments))
+                    .toArray();
+            return (T) constructor.newInstance(argsForConstructor);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    private static Object value(Parameter parameter, List<String> arguments) {
+        Option option = parameter.getAnnotation(Option.class);
+        Class<?> type = parameter.getType();
+        Object o;
+        if (type.equals(int.class)) {
+            int idx = arguments.indexOf("-" + option.value());
+            String s = arguments.get(idx + 1);
+            o = Integer.valueOf(s);
+        } else if (type.equals(String.class)){
+            int idx = arguments.indexOf("-" + option.value());
+            String s = arguments.get(idx + 1);
+            o = String.valueOf(s);
+        } else {
+            boolean contains = arguments.contains("-" + option.value());
+             o = contains;
+        }
+        return o;
     }
 }
--- src/test/java/org/example/ArgsTest.java
+++ src/test/java/org/example/ArgsTest.java
@@ -35,7 +35,6 @@ class ArgsTest {
     }
 
 
-    // TODO string: -d "/usr/logs"
     @Test
     void should_set_string() {
         StringOption parse = Args.parse(StringOption.class, "-d","/usr/logs");
@@ -44,29 +43,31 @@ class ArgsTest {
 
     record StringOption(@Option("d") String directory) {
 
+
     }
 
-    // SAD PATH:
-    // TODO -l t/ -l t f
-    // TODO -p / -p 8080 8081
-    // TODO -d / -d "/usr/logs" "/usr/admin"
-
-    //  default value:
-    // TODO bool: false
-    // TODO int:0
-    // TODO string: ""
-
-
     @Test
-    @Disabled
-    void should_example_1() {
+    void should_parse_multi_option() {
 
-        Options option = Args.parse(Options.class,"-l", "-p", "8080", "-d", "/usr/logs");
+        MultiOptions option = Args.parse(MultiOptions.class,"-l", "-p", "8080", "-d", "/usr/logs");
 
         assertTrue(option.logging());
         assertEquals(8080, option.port());
         assertEquals("/usr/logs", option.dir);
     }
+
+
+    // SAD PATH:
+    // TODO -l t/ -l t f
+    // TODO -p / -p 8080 8081
+
+    // TODO -d / -d "/usr/logs" "/usr/admin"
+    //  default value:
+    // TODO bool: false
+    // TODO int:0
+
+    // TODO string: ""
+
 
 
     @Test
@@ -78,7 +79,7 @@ class ArgsTest {
         assertArrayEquals(new int[]{1, 2, -3, 5}, option.integers);
     }
 
-    static record Options(@Option("l") boolean logging, @Option("p") int port, @Option("d") String dir) {
+    static record MultiOptions(@Option("l") boolean logging, @Option("p") int port, @Option("d") String dir) {
 
     }
 
